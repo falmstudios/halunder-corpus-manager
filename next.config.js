2e840013eea60325690d36601ba87813212fa733
falmstudios <chef@falmstudios.com>
2025-06-18 12:46:13 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  // Remove the experimental appDir option since it's no longer needed in Next.js 14
 }
 
 module.exports = nextConfig
